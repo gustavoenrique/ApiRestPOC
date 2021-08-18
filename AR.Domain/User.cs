948960f16817e263bad683a8fa8c03ec3a6756e1
Gustavo Enrique Mariano <d3v.guss@gmail.com>
2021-08-17 23:42:05 -0300

    Add Domain and DataAccess

---
+++ AR.Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Domain
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(50, ErrorMessage = "{0} max length = 50")]
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public int Age { get; set; }
+    }
+}
